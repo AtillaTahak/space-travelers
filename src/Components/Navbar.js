27fa974f2cdc5940997fb832741b903596520335
Mwila Kaunda <mbkcust@gmail.com>
2021-12-21 14:48:23 +0200

    Style Navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,26 +4,36 @@ import logo from '../logo.png';
 
 const Navbar = () => (
   <div className="header">
-    <div className="header-left">
-      <image src={logo} />
+    <div className="container">
+      <div className="grid items-center margin-y-xs">
+        <div className="col-6 header-left flex items-center gap-sm">
+          <img alt="App logo" className="logo" src={logo} />
+          <p>Space Travelers&apos; Hub</p>
+        </div>
+        <div className="col-6 flex justify-end">
+          <nav>
+            <ul className="flex gap-sm">
+              <li>
+                <NavLink to="/">Rockets</NavLink>
+              </li>
+              <li>
+                <NavLink to="/Missions">Missions</NavLink>
+              </li>
+              <li>
+                <NavLink to="/Dragons">Dragons</NavLink>
+              </li>
+              <li>
+                |
+              </li>
+              <li>
+                <NavLink to="/MyProfile">My Profile</NavLink>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
     </div>
-    <nav>
-      <ul>
-        <li>
-          <NavLink to="/">Rockets</NavLink>
-        </li>
-        <li>
-          <NavLink to="/Missions">Missions</NavLink>
-        </li>
-        <li>
-          <NavLink to="/Dragons">Dragons</NavLink>
-        </li>
-        <li>
-          <NavLink to="/MyProfile">My Profile</NavLink>
-        </li>
-      </ul>
-    </nav>
-
+    <hr />
   </div>
 );
 
