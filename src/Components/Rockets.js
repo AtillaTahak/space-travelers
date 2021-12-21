1575ede192167e8062448715108d49e6e9802736
Atilla Taha Kördüğüm <atillatahaa@gmail.com>
2021-12-22 02:20:34 +0300

    Recive data from api

--- src/Components/Rockets.js
+++ src/Components/Rockets.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { rocketsAction } from '../Redux/rockets/rockets';
 
-const Rockets = () => (
-  <div>
-    <span>hjhdhjdhfjhb</span>
-  </div>
-);
+const Rockets = () => {
+  const reducerRock = useSelector((state) => state.rockets);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(rocketsAction());
+  }, []);
+
+  return (
+    <div>
+      <ul>
+        {reducerRock.data && reducerRock.data.map((e) => (
+          <li key={e.id}>{e.company}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default Rockets;
--- src/Redux/rockets/rockets.js
+++ src/Redux/rockets/rockets.js
@@ -1,15 +1,32 @@
-const ROCKETS = 'ROCKETS';
-const initialRocketState = [{
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
-}];
+export const rocketsAction = createAsyncThunk('rocket/rockets', async () => {
+  const req = await fetch('https://api.spacexdata.com/v3/rockets');
+  const rockets = await req.json();
+  return rockets;
+});
 
-const rocketsReducer = (state = initialRocketState, action) => {
-  switch (action.type) {
-    case ROCKETS:
-      return action.payload;
-    default:
-      return state;
-  }
-};
+const rocketsSlice = createSlice({
+  name: 'rocket',
+  initialState: {
+    data: [],
+    loading: false,
+    error: '',
+  },
+  extraReducers: (builder) => {
+    builder.addCase(rocketsAction.pending, (state) => {
+      state.loading = true;
+      state.error = '';
+    });
+    builder.addCase(rocketsAction.fulfilled, (state, action) => {
+      state.data = action.payload;
+      state.loading = false;
+    });
+    builder.addCase(rocketsAction.rejected, (state) => {
+      state.loading = false;
+      state.error = 'Error fetching user data';
+    });
+  },
+});
 
-export default rocketsReducer;
+export default rocketsSlice.reducer;
