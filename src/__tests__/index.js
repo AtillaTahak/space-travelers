986dd1d9b4732101225614962a9ab07196acfa6a
Munsa1 <81932478+Munsa1@users.noreply.github.com>
2021-12-23 17:38:49 +0200

    Add missionPage snapshot test

---
+++ src/__tests__/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { render, unmountComponentAtNode } from 'react-dom';
+import { act } from 'react-dom/test-utils';
+import pretty from 'pretty';
+import { Provider } from 'react-redux';
+import store from '../redux/configureStore';
+import MissionsPage from '../Components/missionpage/index';
+
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement('div');
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+describe('testing the app', () => {
+  it('testing MissionsPage UI', () => {
+    act(() => {
+      render(
+        <Provider store={store}>
+          <MissionsPage />
+        </Provider>, container,
+      );
+    });
+
+    expect(pretty(container.innerHTML)).toMatchSnapshot();
+  });
+});
